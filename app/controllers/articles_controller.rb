13204a7439ed4b4dcce6077640d41dcb19243e1c
Tomomi <madeinwatashi@gmail.com>
2017-09-30 16:27:30 +1000

    Complete CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy] 
+
     def index
-        @articles = Article.all
+        @articles = Article.all.order('created_at DESC')
     end
 
     def show
-        @article = Article.find(params[:id])
+        # set_article
     end
 
     def new
@@ -12,7 +14,7 @@ class ArticlesController < ApplicationController
     end    
     
     def edit
-        @article = Article.find(params[:id])  
+        # @article = Article.find(params[:id])  
     end
 
     def create
@@ -21,22 +23,32 @@ class ArticlesController < ApplicationController
         if @article.save
             redirect_to article_path(@article)
         else
-            render :new
+            render 'new'
         end        
     end
     
     def update
-        @article = Article.find(params[:id])
+        # @article = Article.find(params[:id])
         if @article.update(article_params)
             redirect_to article_path(@article)
         else
-            reder 'edit'            
+            render 'edit'            
         end
+    end
+
+    def destroy
+        # @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to @article
     end
 
     private
     def article_params
         params.require(:article).permit(:title, :text)
     end
-    
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
 end
